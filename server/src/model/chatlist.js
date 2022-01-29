d28d50a7bab50aaaca580b3b3a0610b8d5836eee
Pdom <ahb0327@naver.com>
2022-01-29 22:02:47 +0900

    Update comments

--- server/src/model/chatlist.js
+++ server/src/model/chatlist.js
@@ -1,20 +1,20 @@
 ﻿module.exports = function(sequelize, DataTypes){
     return sequelize.define('chatlist',{
-        id: {
+        id: { // 채팅의 ID
             type: DataTypes.INTEGER,
             autoIncrement: true,
             primaryKey: true,
             allwNull: false
         },
-        sender: {
+        sender: { // 채팅을 보낸 사람의 닉네임 / 'bot'은 특별 취급
             type: DataTypes.STRING(32),
             allwNull: false
         },
-        type: {
+        type: { // 'text' or 'image'
             type: DataTypes.STRING(32),
             allwNull: false
         },
-        data: {
+        data: { // type의 따른 데이터
             type: DataTypes.TEXT
         }
     })
